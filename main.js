19126cdfd95a83068afb60576f0b665b16b33c6b
Mr Dee <ukattahdivine603@gmail.com>
2025-07-08 18:22:10 +0100

    feature: I added the contact me form/page and Footer

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-document.getElementById('whatsapp-form').addEventListener('submit', function(e) {
-  e.preventDefault();
+// document.getElementById('whatsapp-form').addEventListener('submit', function(e) {
+//   e.preventDefault();
 
-  const phoneNumber = '2347078747186'; // Replace with your actual WhatsApp number
-  const message = encodeURIComponent(document.getElementById('message').value);
+//   const phoneNumber = '2347078747186';
+//   const message = encodeURIComponent(document.getElementById('message').value);
 
-  const whatsappURL = `https://wa.me/${phoneNumber}?text=${message}`;
-  window.open(whatsappURL, '_blank');
-});
+//   const whatsappURL = `https://wa.me/${phoneNumber}?text=${message}`;
+//   window.open(whatsappURL, '_blank');
+// });
+
