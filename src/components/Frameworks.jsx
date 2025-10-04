6464be9a83825a8c2ae5308678a9454bddb3ea5d
Ukattah-Divine603 <ukattahdivine603@gmail.com>
2025-10-04 19:53:05 +0100

    Updates

--- src/components/Frameworks.jsx
+++
@@ -1,9 +0,0 @@
-export default function Frameworks(props) {
-    return(
-        <>
-        <div className="frameworks">
-<p>{props.name}</p>
-        </div>
-        </>
-    )
-}
--- src/components/Icon.jsx
+++
@@ -1,9 +0,0 @@
-export default function Icon(props) {
-    return(
-        <>
-        <div className="icon">
-          <a href={props.href}>{props.svg}</a>
-        </div>
-        </>
-    )
-}
